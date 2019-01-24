1ff10e98a40c6a32ed9baeb808e2e40c9dc176a8
Lorenzo Scott Bertolini <39827031+lorenzoscottb@users.noreply.github.com>
2019-01-24 15:48:26 +0000

    Update apt_reduction.py

--- apt_reduction.py
+++ apt_reduction.py
@@ -2,39 +2,8 @@
 from Keras_GloVe import Model
 import numpy as np
 import os
+import utils
 from apt_toolkit.utils import vector_utils as vu
-
-
-def np_tensor(x, y):
-
-    import numpy as np
-
-    return np.random.uniform(low=-1, high=1, size=(x, y))
-
-
-def write_context_embeddings(models):
-
-    print('Writing down context embeddings...')
-    context_vocabulary = models[-1]._Model__context_words # last to train
-    context_embeddings = np.array((models[-1]).get_weights()[0])
-    file = open(os.getcwd()+'/context_embeddigns.txt', 'w')
-    for index, word in enumerate(context_vocabulary):
-        file.write('en_'+word+' '+str(list(context_embeddings[index])).replace(',', '').strip('[').strip(']')+'\n')
-        # break
-    file.close()
-
-
-def write_focal_embeddings(models):
-
-    for index, model in enumerate(models):
-        print('writing down focal embeddigns for', model.model_name, '...')
-        vocab = models[index].words
-        embeddings = model.get_weights(layer_name='central_embeddings')[0]
-        file = open(os.getcwd()+'/'+paths[index]+'_focal_embeddigns.txt', 'w')
-        for word in vocab:
-            file.write('en_' + word + ' ' + str(list(embeddings[index])).replace(',', '').strip('[').strip(']') + '\n')
-            # break
-        file.close()
 
 
 # model settings
@@ -47,8 +16,6 @@ sub_epochs = len(paths)
 dim = 10
 max_vocab = 10000000
 
-
-# 96920 actual token
 vectors = 'path/to/vectors'
 
 # vectors = vu.load_vector_cache(vector_in_file=vectors)
@@ -71,7 +38,7 @@ nsubj_model.asimmetric_glove(dim)
 models = [amod_model, dobj_model, nsubj_model]
 
 print('\n')
-shared_emb = np_tensor(amod_model.dict_size, dim)
+shared_emb = utils.np_tensor(amod_model.dict_size, dim)
 
 for epoch in range(epochs):
 
@@ -93,6 +60,6 @@ for epoch in range(epochs):
                     512)
         shared_emb = np.array(model.get_weights()[0].reshape(model.dict_size, dim))
 
-write_context_embeddings(models)
-write_focal_embeddings(models)
+utils.write_context_embeddings(models)
+utils.write_focal_embeddings(models)
 
