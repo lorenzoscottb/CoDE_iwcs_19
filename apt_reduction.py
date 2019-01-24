d64e2b00477c21fe7137751c18470e9bf392c4fa
Lorenzo Scott Bertolini <39827031+lorenzoscottb@users.noreply.github.com>
2019-01-24 16:00:08 +0000

    Update apt_reduction.py

--- apt_reduction.py
+++ apt_reduction.py
@@ -7,12 +7,7 @@ from apt_toolkit.utils import vector_utils as vu
 
 
 # model settings
-test_range = 500
-cwd = os.getcwd()
-paths = ['amod', 'dobj', 'nsubj']
 ideal_epochs = 100
-epochs = (int(ideal_epochs/len(paths)))*len(paths)
-sub_epochs = len(paths)
 dim = 10
 max_vocab = 10000000
 
@@ -37,28 +32,7 @@ nsubj_model.asimmetric_glove(dim)
 
 models = [amod_model, dobj_model, nsubj_model]
 
-print('\n')
-shared_emb = utils.np_tensor(amod_model.dict_size, dim)
-
-for epoch in range(epochs):
-
-    print('Global epoch ', epoch+1)
-    if epoch == epochs:
-        last_train_status = True
-
-    for sub_epoch in range(sub_epochs):
-
-        print('training %s model' % paths[sub_epoch])
-        # print(sub_epoch+1)
-
-        model = models[sub_epoch]
-        model.set_weight(shared_emb)
-        i_s, j_s, counts = models[sub_epoch].training_set()
-        model.train([np.array(i_s), np.array(j_s)],
-                    np.array(counts),
-                    sub_epochs,
-                    512)
-        shared_emb = np.array(model.get_weights()[0].reshape(model.dict_size, dim))
+utils.shared_training(ideal_epochs, dim, 100)
 
 utils.write_context_embeddings(models)
 utils.write_focal_embeddings(models)
