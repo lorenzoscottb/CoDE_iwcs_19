6a67b7bc6a638bb77c580fdcf54d4306b9372058
Lorenzo Scott Bertolini <39827031+lorenzoscottb@users.noreply.github.com>
2019-01-24 15:45:53 +0000

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,26 @@
+
+import os
+
+def write_context_embeddings(models):
+
+    print('Writing down context embeddings...')
+    context_vocabulary = models[-1]._Model__context_words # last to train
+    context_embeddings = np.array((models[-1]).get_weights()[0])
+    file = open(os.getcwd()+'/context_embeddigns.txt', 'w')
+    for index, word in enumerate(context_vocabulary):
+        file.write('en_'+word+' '+str(list(context_embeddings[index])).replace(',', '').strip('[').strip(']')+'\n')
+        # break
+    file.close()
+
+
+def write_focal_embeddings(models):
+
+    for index, model in enumerate(models):
+        print('writing down focal embeddigns for', model.model_name, '...')
+        vocab = models[index].words
+        embeddings = model.get_weights(layer_name='central_embeddings')[0]
+        file = open(os.getcwd()+'/'+model.model_name+'_focal_embeddigns.txt', 'w')
+        for word in vocab:
+            file.write('en_' + word + ' ' + str(list(embeddings[index])).replace(',', '').strip('[').strip(']') + '\n')
+            # break
+        file.close()
