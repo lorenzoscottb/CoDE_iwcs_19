334479d7028d25e1cbaa203507daf2a605143883
Lorenzo Scott Bertolini <39827031+lorenzoscottb@users.noreply.github.com>
2019-02-07 14:38:53 +0000

    Update evaluate_vectors.py

--- evaluate_vectors.py
+++ evaluate_vectors.py
@@ -73,16 +73,32 @@ noun_emb = lambda n: np.concatenate((path_noun(n, ctx_em, '', dot=''),
                                      path_noun(n, nsubj_em, 'nsubj')))
 
 
-def run_original_ml_10(vectors, test_phrase='adjectivenouns', composition='concatenation'):
+
+def run_apt_ml_10(test_phrase='adjectivenouns', composition='add', plot=True):
+
+
+    # 500 actual token
+    sample_vectors = '/Users/lb540/Documents/corpora/vectors/apt_vec/' \
+                     'apt_sample.dill'
+
+    # 96920 actual token
+    mix_vectors = '/Users/lb540/Documents/corpora/vectors/apt_vec/' \
+                  'bnc_wiki_toronto_ukwac_gigaword_lc_2_lemma-True_pos-False_n' \
+                  'n_nu_mc-10_mf-50_cds-1.0_k-1_constant_pmi.dill'
+
+    vectors = vu.load_vector_cache(vector_in_file=mix_vectors)
 
     # test_phrase = 'adjectivenouns'
     # composition = 'add'
     df = pd.read_csv(ml_10)
     ml_values = []
     cs_values = []
-    for index, element in enumerate(df.values):
-        e = element[0].split()
-        if e[1] != test_phrase:
+
+    if test_phrase == 'all':
+        test_phrase = list(set(df['type']))
+
+    for index, e in enumerate(df.values):
+        if e[1] not in [test_phrase]:
             continue
         try:
             if composition == 'add':
@@ -92,7 +108,7 @@ def run_original_ml_10(vectors, test_phrase='adjectivenouns', composition='conca
                 adj_v_2 = vu.create_offset_vector(vector=vectors[e[5]], offset_path='amod')
                 nn_v_2 = vectors[e[6]]
                 composed_v_2 = mozart.union_apts(vector_1=adj_v_2, vector_2=nn_v_2)
-    
+
             if composition == 'intersect':
                 adj_v_1 = vu.create_offset_vector(vector=vectors[e[3]], offset_path='amod')
                 nn_v_1 = vectors[e[4]]
@@ -100,7 +116,7 @@ def run_original_ml_10(vectors, test_phrase='adjectivenouns', composition='conca
                 adj_v_2 = vu.create_offset_vector(vector=vectors[e[5]], offset_path='amod')
                 nn_v_2 = vectors[e[6]]
                 composed_v_2 = mozart.intersect_apts(vector_1=adj_v_2, vector_2=nn_v_2)
-    
+
             if composition == 'path_intersect':
                 adj_v_1 = vu.create_offset_vector(vector=vectors[e[3]], offset_path='amod')
                 nn_v_1 = vectors[e[4]]
@@ -108,7 +124,7 @@ def run_original_ml_10(vectors, test_phrase='adjectivenouns', composition='conca
                 adj_v_2 = vu.create_offset_vector(vector=vectors[e[5]], offset_path='amod')
                 nn_v_2 = vectors[e[6]]
                 composed_v_2 = mozart.path_intersect_apts(vector_1=adj_v_2, vector_2=nn_v_2)
-    
+
             ml_values.append(int(e[-1]))
             cs_values.append(vu.calculate_similarity(composed_v_1,
                                                      composed_v_2,
@@ -117,20 +133,31 @@ def run_original_ml_10(vectors, test_phrase='adjectivenouns', composition='conca
 
         except:
             print('%s:something thing whent wrong' % e[3:7])
+            break
 
     print('oroginal vectors ', spearmanr(ml_values, cs_values))
 
+    if plot:
+        import seaborn as sns
 
-def run_reduced_ml_10(test_phrase='adjectivenouns', composition='concatenation'):
+        sns.set(style="whitegrid")
+        sns.scatterplot(ml_values, cs_values)
 
-#   test_phrase='adjectivenouns'
-#   composition='concatenation'
+
+def run_reduced_apt_ml_10(test_phrase='adjectivenouns', composition='concatenation', plot=True):
+
+    #   test_phrase='adjectivenouns'
+    #   composition='concatenation'
+
     df = pd.read_csv(ml_10)
     ml_values = []
     cs_values = []
-    for index, element in enumerate(df.values):
-        e = element[0].split()
-        if e[1] != test_phrase:
+
+    if test_phrase == 'all':
+        test_phrase = list(set(df['type']))
+
+    for index, e in enumerate(df.values):
+        if e[1] not in [test_phrase]:
             continue
         try:
             # some useful debug
@@ -154,7 +181,13 @@ def run_reduced_ml_10(test_phrase='adjectivenouns', composition='concatenation')
 
     print('reduced vectors ', spearmanr(ml_values, cs_values))
 
+    if plot:
+        import seaborn as sns
 
-run_original_ml_10(vectors)
-run_reduced_ml_10()
+        sns.set(style="whitegrid")
+        sns.scatterplot(ml_values, cs_values)
+
+
+run_apt_ml_10()
+run_reduced_apt_ml_10()
 
