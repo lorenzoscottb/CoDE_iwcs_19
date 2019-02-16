26544577657d7488b646a86955467aa40c37efb0
Lorenzo Scott Bertolini <39827031+lorenzoscottb@users.noreply.github.com>
2019-02-16 15:57:00 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 
 import os
 
+
 def np_tensor(x, y):
 
-    import numpy as np
-
     return np.random.uniform(low=-1, high=1, size=(x, y))
+
 
 def write_context_embeddings(models):
 
@@ -19,44 +19,15 @@ def write_context_embeddings(models):
     file.close()
 
 
-def write_focal_embeddings(models):
+def write_focal_embeddings(models, paths):
 
     for index, model in enumerate(models):
         print('writing down focal embeddigns for', model.model_name, '...')
-        vocab = models[index].words
+        vocab = models[index].focal_words
         embeddings = model.get_weights(layer_name='central_embeddings')[0]
-        file = open(os.getcwd()+'/'+model.model_name+'_focal_embeddigns.txt', 'w')
-        for word in vocab:
-            file.write('en_' + word + ' ' + str(list(embeddings[index])).replace(',', '').strip('[').strip(']') + '\n')
+        file = open(os.getcwd()+'/'+paths[index]+'_focal_embeddigns.txt', 'w')
+        for i, word in enumerate(vocab):
+            file.write('en_' + word + ' ' + str(list(embeddings[i-1])).replace(',', '').strip('[').strip(']') + '\n')
             # break
         file.close()
-     
-def shared_training(models, emdeddings_dimensions, epochs)
-    
-    ideal_epochs = epochs
-    epochs = (int(ideal_epochs/len(paths)))*len(paths)
-    sub_epochs = len(models)
-
-    print('\n')
-    shared_emb = np_tensor(amod_model.dict_size, emdeddings_dimensions)
-
-    for epoch in range(epochs):
-
-        print('Global epoch ', epoch+1)
-#         if epoch == epochs:
-#             last_train_status = True
-
-        for sub_epoch in range(sub_epochs):
-
-            print('training %s model' % paths[sub_epoch])
-            # print(sub_epoch+1)
-
-            model = models[sub_epoch]
-            model.set_weight(shared_emb)
-            i_s, j_s, counts = models[sub_epoch].training_set()
-            model.train([np.array(i_s), np.array(j_s)],
-                        np.array(counts),
-                        sub_epochs,
-                        512)
-            shared_emb = np.array(model.get_weights()[0].reshape(model.dict_size, dim))
         
